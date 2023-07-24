40f1b11f55a410c1a8fdbd2f65f76861aee691cd
AckimJnr <acklongwe@gmail.com>
2023-07-24 11:37:28 +0200

    Add bubblesort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * bubble_sort - sorts an array using the bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp = 0;
+	int swapped = 0;
+
+	i = 0, j = 0;
+
+	if (array == NULL || size <= 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+
+		if (swapped <= 0)
+			return;
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Implementantion of sorting algorithms in C
+
+## Algorithms you will find in this repository
+- Bubblesort Algorithm
+- Insertion Sort
+- Selection Sort
