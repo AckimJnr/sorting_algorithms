9ff06d131c2287394b97eb5632d215133cef72c5
AckimJnr <acklongwe@gmail.com>
2023-07-25 15:41:48 +0200

    Add selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array using selection sort
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: Returns void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min_item_index = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (i < size - 1)
+	{
+		min_item_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_item_index])
+				min_item_index = j;
+		}
+		if (min_item_index != i)
+			swap(&array[i], &array[min_item_index]);
+		print_array(array, size);
+		i++;
+	}
+}
+/**
+ * swap - swaps integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	if (*a == *b)
+		return;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
--- sort.h
+++ sort.h
@@ -14,7 +14,6 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -22,5 +21,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *, int *);
-
 #endif
