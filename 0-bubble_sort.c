0d53746d120a286e21a939e97a0e9155032e9580
AckimJnr <acklongwe@gmail.com>
2023-07-25 15:05:07 +0200

    Incorporate swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,3 +34,20 @@ void bubble_sort(int *array, size_t size)
 			return;
 	}
 }
+/**
+ * swap - swaps integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	if (*a == *b)
+		return;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
